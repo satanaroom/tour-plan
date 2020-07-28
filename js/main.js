4681af92833aad758d14c24135e201f02188d2a6
satanaroom <oficialbalunov@gmail.com>
2020-07-29 01:45:00 +0800

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -56,3 +56,11 @@ function init() {
   )),
     myMap.geoObjects.add(myGeoObject);
 }
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
