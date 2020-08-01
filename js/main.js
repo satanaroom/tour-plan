8bd8ee3980022d7206c49f408655a66ef2133a40
satanaroom <oficialbalunov@gmail.com>
2020-08-02 01:21:18 +0800

    create: modal dialogs

--- js/main.js
+++ js/main.js
@@ -1,66 +1,84 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-
-  // Перелистывание с клавиатуры
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-
-  // Перелистывание с клавиатуры
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [37.58903142, 126.93428341],
-    zoom: 12,
-  });
-  (myGeoObject = new ymaps.GeoObject(
-    {
-      geometry: {
-        type: "Point",
-        coordinates: [37.58903142, 126.93428341],
-      },
-      properties: {
-        iconContent: "Grand Hilton Hotel",
-        hintContent: "Grand Hilton Hotel",
-      },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
     },
-    {
-      preset: "islands#blackStretchyIcon",
-      draggable: true,
-    }
-  )),
-    myMap.geoObjects.add(myGeoObject);
-}
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+    // Перелистывание с клавиатуры
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+
+    // Перелистывание с клавиатуры
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [37.58903142, 126.93428341],
+      zoom: 12,
+    });
+    (myGeoObject = new ymaps.GeoObject(
+      {
+        geometry: {
+          type: "Point",
+          coordinates: [37.58903142, 126.93428341],
+        },
+        properties: {
+          iconContent: "Grand Hilton Hotel",
+          hintContent: "Grand Hilton Hotel",
+        },
+      },
+      {
+        preset: "islands#blackStretchyIcon",
+        draggable: true,
+      }
+    )),
+      myMap.geoObjects.add(myGeoObject);
+  }
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
