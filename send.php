af9019bb16e1f21c46f7ad218d58fb1fa29a3916
satanaroom <oficialbalunov@gmail.com>
2020-08-01 11:51:14 +0800

    creat: send message in footer

--- send.php
+++ send.php
@@ -6,17 +6,16 @@ require 'phpmailer/Exception.php';
 
 // Переменные, которые отправляет пользователь
 $name = $_POST['name'];
-$email = $_POST['email'];
-$text = $_POST['text'];
-$file = $_FILES['myfile'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
 // Формирование самого письма
-$title = "Заголовок письма";
+$title = "Новое обращение Best Tour plan";
 $body = "
-<h2>Новое письмо</h2>
+<h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
-<b>Почта:</b> $email<br><br>
-<b>Сообщение:</b><br>$text
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
 ";
 
 // Настройки PHPMailer
@@ -31,30 +30,16 @@ try {
   };
 
   // Настройки вашей почты
-  $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
-  $mail->Username   = 'your_login'; // Логин на почте
-  $mail->Password   = 'password'; // Пароль на почте
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = 'oficialbalunov@gmail.com'; // Логин на почте
+  $mail->Password   = 'gaz_UMU43'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
-  $mail->setFrom('mail@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+  $mail->setFrom('oficialbalunov@gmail.com', 'Андрей Балунов'); // Адрес самой почты и имя отправителя
 
   // Получатель письма
-  $mail->addAddress('youremail@yandex.ru');
-  $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+  $mail->addAddress('oficialsatana@mail.ru');
 
-  // Прикрипление файлов к письму
-  if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-      $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-      $filename = $file['name'][$ct];
-      if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-        $mail->addAttachment($uploadfile, $filename);
-        $rfile[] = "Файл $filename прикреплён";
-      } else {
-        $rfile[] = "Не удалось прикрепить файл $filename";
-      }
-    }
-  }
   // Отправка сообщения
   $mail->isHTML(true);
   $mail->Subject = $title;
@@ -72,4 +57,4 @@ try {
 }
 
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+header('Location:thankyou.html');
