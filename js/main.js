c365733c424ded2df32561cf12eba2a964f419bf
satanaroom <oficialbalunov@gmail.com>
2020-08-09 20:34:17 +0800

    create: phone mask

--- js/main.js
+++ js/main.js
@@ -86,8 +86,5 @@ $(document).ready(function () {
   });
   AOS.init();
 
-  $.mask, (definitions["-"] = "[A-Fa-f0-9]");
-  jQuery(function ($) {
-    $("input[type='tel']").mask("+7(999)999-99-99");
-  });
+  $(".phone_us").mask("+7 (000) 000-00-00");
 });
